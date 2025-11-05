8e98604f38612b2773a4137bc87ae45863da8f86
pharbst <102487579+pharbst@users.noreply.github.com>
2025-11-05 18:02:19 +0100

    Send ICMP echo request to localhost

--- src/main.c
+++ src/main.c
@@ -1,98 +1,146 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <stdio.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
 #include <errno.h>
+#include <netinet/ip_icmp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-typedef struct s_stats {
-	char *target;
-	int transmitted;
-	int received;
-	int loss;
-	int time;
-	float min;
-	float avg;
-	float max;
-	float mdev;
-} t_stats;
+#define ICMP_PAYLOAD "ft_ping test payload"
+#define EPOLL_TIMEOUT_MS 1000
 
-// void sigint_handler(int signum);
-// void close_socket(int action);
-// void print_stats(t_stats stats);
+static uint16_t icmp_checksum(const void *data, size_t len) {
+    const uint16_t *words = data;
+    uint32_t sum = 0;
 
-// void sigint_handler(int signum) {
-// 	// printf("INTERRUPT PING\n");
-// 	// // print_stats();
-// 	// close_socket(-1);
-// 	// exit(signum + 0x80);
-// }
+    while (len > 1) {
+        sum += *words++;
+        len -= 2;
+    }
 
-// void print_stats(t_stats stats) {
-// 	printf("--- %s ping statistics ---\n", stats.target);
-// 	printf("%d packets transmitted, %d received, %d%% packet loss, time %dms\n",
-// 		stats.transmitted, stats.received, stats.loss, stats.time);
-// 	printf("rtt min/avg/max/mdev = %.3f/%.3f/%.3f/%.3f ms\n",
-// 		stats.min, stats.avg, stats.max, stats.mdev);
-// }
+    if (len == 1) {
+        uint16_t last = 0;
+        *(uint8_t *)&last = *(const uint8_t *)words;
+        sum += last;
+    }
 
-// int main() {
-// 	int sockfd;
+    while (sum >> 16) {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
 
-// 	if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
-// 		return (perror("socket creation failed\n"), 1);
-// 	struct icmphdr head;
-// 	// bezero(&head, sizeof(head));
-// 	head.type = ICMP_ECHO;
-// 	head.code = 0;
-// 	// head.checksum =
-// 	struct sockaddr_in addr;
-// 	inet_pton(AF_INET, "127.0.0.1", &addr);
-// 	ssize_t bytes =  sendto(sockfd, &head, sizeof(head), 0, (struct sockaddr*)&addr, sizeof(&addr));
-// 	printf("%li %s\n",bytes, strerror(errno));
-
-// 	struct iovec urmum;
-// 	struct msghdr urdad;
-// 	urdad.msg_iov = &urmum;
-// 	printf("ur dad is %ld thick\n", recvmsg(sockfd, &urdad, 0));
-// 	return 0;
-// }
-
-#define NO_ARG 0
-
-void print_help(int status);
-
-int main(int argc, char **argv) {
-	int		icmp_socket;
-
-	if (argc == 1)
-		return (print_help(NO_ARG), 1);
-	icmp_socket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-	if (icmp_socket < 0)
-		return (icmp_socket);
-	
-	return (0);
+    return (uint16_t)(~sum);
 }
 
-void print_help(int status) {
-	switch (status) {
-		case 0:
-			printf("ft_ping: usage error: destination address required\n");
-			break;
-		// case 1:
-		// case 2:
-		// case 3:
-		// case 4:
-		// case 5:
-		default:
-			printf("ft_ping: undefined error %d\n", status);
-			break;
-	}
+static void print_buffer(const char *label, const void *buf, size_t len) {
+    const unsigned char *bytes = buf;
+
+    printf("%s (%zu bytes):\n", label, len);
+    for (size_t i = 0; i < len; ++i) {
+        if (i % 16 == 0) {
+            printf("%04zx: ", i);
+        }
+        printf("%02x ", bytes[i]);
+        if ((i + 1) % 8 == 0 && (i + 1) % 16 != 0) {
+            printf(" ");
+        }
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    if (len % 16 != 0) {
+        printf("\n");
+    }
+    fflush(stdout);
+}
+
+int main(void) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd == -1) {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+
+    unsigned char packet[sizeof(struct icmphdr) + sizeof(ICMP_PAYLOAD) - 1];
+    memset(packet, 0, sizeof(packet));
+
+    struct icmphdr *icmp = (struct icmphdr *)packet;
+    icmp->type = ICMP_ECHO;
+    icmp->code = 0;
+    icmp->un.echo.id = htons((uint16_t)getpid());
+    icmp->un.echo.sequence = htons(1);
+
+    memcpy(packet + sizeof(struct icmphdr), ICMP_PAYLOAD, sizeof(ICMP_PAYLOAD) - 1);
+    icmp->checksum = 0;
+    icmp->checksum = icmp_checksum(packet, sizeof(packet));
+
+    struct sockaddr_in target;
+    memset(&target, 0, sizeof(target));
+    target.sin_family = AF_INET;
+    target.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    ssize_t sent = sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr *)&target,
+                          sizeof(target));
+    if (sent < 0) {
+        perror("sendto");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    print_buffer("ICMP echo request", packet, (size_t)sent);
+
+    int epfd = epoll_create1(0);
+    if (epfd == -1) {
+        perror("epoll_create1");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    struct epoll_event event;
+    memset(&event, 0, sizeof(event));
+    event.events = EPOLLIN;
+    event.data.fd = sockfd;
+
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &event) == -1) {
+        perror("epoll_ctl");
+        close(epfd);
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    struct epoll_event fired;
+    int nfds = epoll_wait(epfd, &fired, 1, EPOLL_TIMEOUT_MS);
+    if (nfds == -1) {
+        perror("epoll_wait");
+        close(epfd);
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    if (nfds == 0) {
+        fprintf(stderr, "timeout waiting for ICMP reply\n");
+        close(epfd);
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    unsigned char response[1024];
+    ssize_t received = recv(sockfd, response, sizeof(response), 0);
+    if (received < 0) {
+        perror("recv");
+        close(epfd);
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    print_buffer("ICMP response", response, (size_t)received);
+
+    close(epfd);
+    close(sockfd);
+
+    return EXIT_SUCCESS;
 }
